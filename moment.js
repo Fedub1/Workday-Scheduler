c49d65ee76eb9ef79513ef89896c39f92a995b61
Felicia Wright <fedub1@yahoo.com>
2020-10-03 15:21:20 -0400

    Updated HTML

--- moment.js
+++ moment.js
@@ -1,44 +1,71 @@
 
-var moment = moment();
-console.log(moment);
-console.log(moment.format("dddd, MMMM Do YYYY, h:mm:ss a"));
+var m = moment();
+console.log(m);
+console.log(m.format("dddd, MMMM Do YYYY, h:mm:ss a"));
 //var currentDay = ("#currentDay");
-document.getElementById('currentDay').innerHTML = moment;
+document.getElementById('currentDay').innerHTML = m;
 //moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
 
-var moment = Time;
-Time = currentHour
-$(".row").display(function(){
-  const currentHour = moment().hour();
-  const rowHour = Number($(this).attr("id"))
+//var mmoment = Time;
+//Time = currentHour
+$(".row").each(function(){
+  const currentHour = parseInt(m.format("H"));
+  const rowHour = parseInt($(this).attr("id"));
+console.log(currentHour);
+
    $(this).addClass("past")
 if (rowHour === currentHour) {
-  $(this).addClass("present")
+  $(this).addClass("present");
 } else if (rowHour<currentHour){
-  $(this).addClass("past")  
+  $(this).addClass("past");  
 
-} else if (rowHour>currentHour){
-  $(this).addClass("future")
+} else if (rowHour>currentHour) {
+  $(this).addClass("future");
 }
-}) 
 
-$(".eventButton").click(function(){
+$(".saveBtn").click(function(){
   const id = $(this).attr("id")
   const inputId = id + "Input"
   localStorage.setItem(inputId, $(`#${inputId}`).val())
 })
 
 $(document).ready(function() {
+ 
+ const description = document.getElementById("description");
+ const saveBtn = document.getElementById("hour");
+ 
   // check if item is in localStorage for all inputs
+//function description(){
+  //const description = description.value;
+//}
+
+  hour.onclick = function(){
+
+ const value = text.value;
+ console.log ("description");
+  console.log ("text");
+
+
+  if (description && hour) {
+    localStorage.SetItem(description, text);
+
+  }
+  };
+  for (let i = 0; i <localStorage.length; i++){
+const description = localStorage.description(i);
+const value = localStorage.getItem(description);
+hour.innerHTML +=`${description}: ${text}<br />`;
+  }
+
+  //localStorage.setItem('inputId', 'text')
   
-  localStorage.setItem('inputID', 'value')
-  
-  localStorage.getItem('inputID');
+  //localStorage.getItem ("inputId")
+ // localStorage.getItem('inputID');
   // if it is in local storage, put the value into the input field
   // $(`#${inputId}`).val(item)
 })
-//store inputId();
+//store description();
   //render inputId();
 function setColor(element, color) {
  element.style.backgroundColor = color;
-}
+}});
