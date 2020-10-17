ea3f0c83749843692fa7c3413d4f6d5f51a743ae
Felicia Wright <fedub1@yahoo.com>
2020-10-16 22:41:02 -0400

    Updated Readme

--- moment.js
+++ moment.js
@@ -47,45 +47,7 @@ rowInput.addEventListener('textarea', lettter => {
 
 })
 
- //const saveToLocalStorage = () => {
-//localStorage.setItem("textinput", description.textContent)
 
- 
- //}
-
-//saveBtn.addEventListener("click", saveToLocalStorage)
-  // check if item is in localStorage for all inputs
-//function description(){
-  //const description = description.value;
-//}
-
-  //saveBtn.onclick = function(){
-
- //const value = text.value;
-//console.log (description);
- // console.log (text);
-
-
- // if (description && hour) {
-    //localStorage.setItem('description', 'hour');
-    //location.reload()
- // }
- // };
-  //for (let i = 0; i <localStorage.length; i++){
-//const description = localStorage.description(i);
-//const value = localStorage.getItem(description);
-//hour.innerHTML +=`${description}: ${text}<br />`;
-  //}
-
-  //localStorage.setItem('inputId', 'text')
-  
-  //localStorage.getItem ("inputId")
- // localStorage.getItem('inputID');
-  // if it is in local storage, put the value into the input field
-  // $(`#${inputId}`).val(item)
-//})
-//store description();
-  //render inputId();
 function setColor(element, color) {
  element.style.backgroundColor = color;
 }},)});
